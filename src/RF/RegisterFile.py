a1241f36dc8b100c3e7628612d7f900347b0d6e4
Abonite <378751910@qq.com>
2023-03-04 00:39:35 +0800

    new file:   src/RF/RegisterFile.py 	modified:   src/Decoder/InstructionSet.py 	modified:   src/PC/ProgramCounter.py 	src/PC/PCExceptions.py

---
+++ src/RF/RegisterFile.py
@@ -0,0 +1,11 @@
+class Register:
+    curr_value = 0
+    lable = 0
+
+    def __init__(self, label: int):
+        self.lable = label
+
+
+class RegisterFile:
+    def __init__(self):
+        pass
