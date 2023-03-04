03eb5500ccc6fb82ff2daab13fd328be521fa847
Abonite <378751910@qq.com>
2023-03-04 18:39:22 +0800

    modified:   src/Decoder/InstructionSet.py

--- src/Decoder/InstructionSet.py
+++ src/Decoder/InstructionSet.py
@@ -1,15 +1,11 @@
 from typing import List
 from DecoderExceptions import OperateCodeLenthError, \
-    RegisterLabelBitsCrowdedError, \
-    InstructionLenthError, \
-    RegisterLabelLenthError, \
-    ImmediateNumberBitsCrowdedError, \
-    NotThisInstructionError, \
-    InvalidBitValueError, \
+    RegisterLabelBitsCrowdedError, InstructionLenthError, \
+    RegisterLabelLenthError, ImmediateNumberBitsCrowdedError, \
+    NotThisInstructionError, InvalidBitValueError, \
     RegisterNotBeAllowedError
 
 
-# TODO: how to check?
 class Instruction:
     i_type = ""
     i_name = ""
@@ -104,13 +100,19 @@ class Instruction:
                     self.check_flag[self.inst_lenth - 1 - i] = True
 
     def checkSourceRegisterIsAllowed(self, inst: List[int]):
-        if [inst[self.inst_lenth - i - 1] for i in self.source_register_bits] \
-                not in self.source_register_allowed:
+        if not self.source_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.source_register_bits
+        ] not in self.source_register_allowed:
             raise RegisterNotBeAllowedError(self.i_name, "source register")
 
     def checkTargetRegisterIsAllowed(self, inst: List[int]):
-        if [inst[self.inst_lenth - i - 1] for i in self.target_register_bits] \
-                not in self.target_register_allowed:
+        if not self.source_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.target_register_bits
+        ] not in self.target_register_allowed:
             raise RegisterNotBeAllowedError(self.i_name, "target register")
 
     def checkTargetRegister(self):
@@ -130,6 +132,8 @@ class Instruction:
 class LOAD(Instruction):
     fsource_register_bits = []
     ssource_register_bits = []
+    fsource_register_allowed = []
+    ssource_register_allowed = []
 
     def __init__(
         self,
@@ -139,18 +143,25 @@ class LOAD(Instruction):
         target_register: List[int] = [21, 20, 19, 18, 17, 16],
         immediate_number: List[int] = [],
         first_source_register: List[int] = [],
-        second_source_register: List[int] = []
+        second_source_register: List[int] = [],
+        target_register_allowed: List[int] = [],
+        first_source_register_allowed: List[int] = [],
+        second_source_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
             instruction_name=instruction_name,
             op_code=op_code,
             immediate_number=immediate_number,
-            target_register_bits=target_register
+            target_register_bits=target_register,
+            target_register_allowed=target_register_allowed
         )
 
         self.fsource_register_bits = first_source_register
         self.ssource_register_bits = second_source_register
+
+        self.fsource_register_allowed = first_source_register_allowed
+        self.ssource_register_allowed = second_source_register_allowed
 
         self.checkInstructionIntegrity()
 
@@ -199,10 +210,34 @@ class LOAD(Instruction):
                 else:
                     self.check_flag[self.inst_lenth - 1 - i] = True
 
+    def checkFirstSourceRegisterIsAllowed(self, inst: List[int]):
+        if not self.fsource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.fsource_register_bits
+        ] not in self.fsource_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "first source register"
+            )
+
+    def checkSecondSourceRegisterIsAllowed(self, inst: List[int]):
+        if not self.ssource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.ssource_register_bits
+        ] not in self.ssource_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "second source register"
+            )
+
 
 class STORE(Instruction):
     ftarget_register_bits = []
     starget_register_bits = []
+    ftarget_register_allowed = []
+    starget_register_allowed = []
 
     def __init__(
         self,
@@ -211,17 +246,23 @@ class STORE(Instruction):
         op_code: List[int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         source_register: List[int] = [21, 20, 19, 18, 17, 16],
         first_target_register: List[int] = [],
-        second_target_register: List[int] = []
+        second_target_register: List[int] = [],
+        source_register_allowed: List[int] = [],
+        first_target_register_allowed: List[int] = [],
+        second_target_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
             instruction_name=instruction_name,
             op_code=op_code,
-            source_register_bits=source_register
+            source_register_bits=source_register,
+            source_register_allowed=source_register_allowed
         )
 
         self.ftarget_register_bits = first_target_register
         self.starget_register_bits = second_target_register
+        self.ftarget_register_allowed = first_target_register_allowed
+        self.starget_register_allowed = second_target_register_allowed
 
         self.checkInstructionIntegrity()
 
@@ -269,6 +310,28 @@ class STORE(Instruction):
                 else:
                     self.check_flag[self.inst_lenth - 1 - i] = True
 
+    def checkFirstTargetRegisterIsAllowed(self, inst: List[int]):
+        if not self.ftarget_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.ftarget_register_bits
+        ] not in self.ftarget_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "first target register"
+            )
+
+    def checkSecondTargetRegisterIsAllowed(self, inst: List[int]):
+        if not self.source_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.starget_register_bits
+        ] not in self.starget_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "second target register"
+            )
+
 
 class MOVE(Instruction):
     def __init__(
@@ -278,13 +341,17 @@ class MOVE(Instruction):
         op_code: List[int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         source_register: List[int] = [5, 4, 3, 2, 1, 0],
         target_register: List[int] = [21, 20, 19, 18, 17, 16],
+        source_register_allowed: List[int] = [],
+        target_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
             instruction_name=instruction_name,
             op_code=op_code,
             source_register_bits=source_register,
-            target_register_bits=target_register
+            target_register_bits=target_register,
+            source_register_allowed=source_register_allowed,
+            target_register_allowed=target_register_allowed
         )
 
         self.checkInstructionIntegrity()
@@ -298,6 +365,7 @@ class MOVE(Instruction):
 class INTEGER(Instruction):
     asource_register_bits = []
     aimmediate_number_bits = []
+    asource_register_allowed = []
 
     def __init__(
         self,
@@ -308,7 +376,10 @@ class INTEGER(Instruction):
         source_register: List[int] = [],
         another_source_register: List[int] = [],
         immediate_number: List[int] = [],
-        another_immediate_number: List[int] = []
+        target_register_allowed: List[int] = [],
+        source_register_allowed: List[int] = [],
+        another_immediate_number: List[int] = [],
+        another_source_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
@@ -317,10 +388,13 @@ class INTEGER(Instruction):
             target_register_bits=target_register,
             source_register_bits=source_register,
             immediate_number=immediate_number,
+            target_register_allowed=target_register_allowed,
+            source_register_allowed=source_register_allowed
         )
 
         self.asource_register_bits = another_source_register
         self.aimmediate_number_bits = another_immediate_number
+        self.asource_register_allowed = another_source_register_allowed
 
         self.checkInstructionIntegrity()
 
@@ -361,9 +435,21 @@ class INTEGER(Instruction):
                 else:
                     self.check_flag[self.inst_lenth - 1 - i] = True
 
+    def checkASoureceRegisterIsAllowed(self, inst: List[int]):
+        if not self.asource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.asource_register_bits
+        ] not in self.asourece_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "another source register"
+            )
+
 
 class BRANCH(Instruction):
     asource_register_bits = []
+    asource_register_allowed = []
 
     def __init__(
         self,
@@ -373,7 +459,10 @@ class BRANCH(Instruction):
         target_register: List[int] = [21, 20, 19, 18, 17, 16],
         source_register: List[int] = [11, 10, 9, 8, 7, 6],
         another_source_register: List[int] = [],
-        immediate_number: List[int] = []
+        immediate_number: List[int] = [],
+        target_register_allowed: List[int] = [],
+        source_register_allowed: List[int] = [],
+        asource_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
@@ -382,9 +471,12 @@ class BRANCH(Instruction):
             target_register_bits=target_register,
             source_register_bits=source_register,
             immediate_number=immediate_number,
+            target_register_allowed=target_register_allowed,
+            source_register_allowed=source_register_allowed,
         )
 
         self.asource_register_bits = another_source_register
+        self.asource_register_allowed = asource_register_allowed
 
         self.checkInstructionIntegrity()
 
@@ -414,10 +506,23 @@ class BRANCH(Instruction):
                 else:
                     self.check_flag[self.inst_lenth - 1 - i] = True
 
+    def checkASoureceRegisterIsAllowed(self, inst: List[int]):
+        if not self.asource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.asource_register_bits
+        ] not in self.asourece_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "another source register"
+            )
+
 
 class JUMP(Instruction):
     fsource_register_bits = []
     ssource_register_bits = []
+    fsource_register_allowed = []
+    ssource_register_allowed = []
 
     def __init__(
         self,
@@ -426,7 +531,9 @@ class JUMP(Instruction):
         op_code: List[int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         first_source_register: List[int] = [],
         second_source_register: List[int] = [],
-        immediate_number: List[int] = []
+        immediate_number: List[int] = [],
+        first_source_register_allowed: List[int] = [],
+        second_source_register_allowed: List[int] = []
     ):
         super().__init__(
             instruction_type=instruction_type,
@@ -437,6 +544,8 @@ class JUMP(Instruction):
 
         self.fsource_register_bits = first_source_register
         self.ssource_register_bits = second_source_register
+        self.fsource_register_allowed = first_source_register_allowed
+        self.ssource_register_allowed = second_source_register_allowed
 
         self.checkInstructionIntegrity()
 
@@ -482,6 +591,28 @@ class JUMP(Instruction):
                     )
                 else:
                     self.check_flag[self.inst_lenth - 1 - i] = True
+
+    def checkFirstSourceRegisterIsAllowed(self, inst: List[int]):
+        if not self.fsource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.fsource_register_bits
+        ] not in self.fsource_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "first source register"
+            )
+
+    def checkSecondSourceRegisterIsAllowed(self, inst: List[int]):
+        if not self.ssource_register_bits:
+            return
+        elif [
+            inst[self.inst_lenth - i - 1] for i in self.ssource_register_bits
+        ] not in self.ssource_register_allowed:
+            raise RegisterNotBeAllowedError(
+                self.i_name,
+                "second source register"
+            )
 
 
 class Instrucitons:
