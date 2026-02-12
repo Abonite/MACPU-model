65db1f7dcd2ce75448a4af65ce79dea137774234
Abonite <378751910@qq.com>
2026-02-12 16:32:13 +0800

    modified:   src/Decoder/decoder.h

--- src/Decoder/decoder.h
+++ src/Decoder/decoder.h
@@ -1,5 +1,18 @@
 struct decoder {
     unsigned int current_code;
 
-    struct* current_instruction;
+    unsigned short op_code;
+    unsigned char  target_register;
+    unsigned char  source_register_1;
+    unsigned char  source_register_2;
+    unsigned char  immediate_number;
+
+    int (*set_code)(struct decoder *self, unsigned int code);
+    int (*decode)(struct decoder *self);
 };
+
+struct decoder new_decoder() {
+    struct result = decoder {
+        current_code = 0,
+    };
+}
