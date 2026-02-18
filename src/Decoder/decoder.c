6a62fd469373eac6702e4551fe7fefd1446c699d
abonite <abonite>
2026-02-18 12:00:04 +0800

    modified:   src/Decoder/decoder.c 	modified:   src/Decoder/decoder.h 	new file:   src/dynamicstring.h

--- src/Decoder/decoder.c
+++ src/Decoder/decoder.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #define __32bit__
 
 #include "decoder.h"
@@ -9,8 +11,8 @@ struct Decoder new_decoder() {
         0,
         0,
         0,
-        0,
-        meth_decode_impl
+        meth_decode_impl,
+        meth_check_impl
     };
 
     return result;
@@ -20,108 +22,335 @@ struct Decoder new_decoder() {
     #define OP_CODE_LBIT 22 // low bit at 22
     #define OP_CODE_BITM 0b1111111111
 
-    int meth_decode_impl(struct Decoder *self, int source_code) {
-        unsigned char op_code;
+    #define REGA_CODE_LBIT  16 // low bit at 16
+    #define P_CODE_LBIT     12
+    #define REGB_CODE_LBIT  6
+    #define REG_CODE_BITM   0b111111
+    #define P_CODE_BITM     0b1111
+
+    struct CheckResult meth_decode_impl(struct Decoder *self, int source_code) {
+        unsigned short op_code = 0;
         op_code = (source_code & (OP_CODE_BITM << OP_CODE_LBIT)) >> OP_CODE_LBIT;
+        self->reg_A = (source_code & (REG_CODE_BITM << REGA_CODE_LBIT)) >> REGA_CODE_LBIT;
+        self->mid_p = (source_code & (P_CODE_BITM << P_CODE_LBIT)) >> P_CODE_LBIT;
+        self->reg_B = (source_code & (REG_CODE_BITM << REGB_CODE_LBIT)) >> REGB_CODE_LBIT;
+        self->reg_C = source_code & REG_CODE_BITM;
 
         switch (op_code) {
         // LOAD8 immediate
         case 0b0000000001:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 1;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = BIT16_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // LOAD8 register address
         case 0b0000000010:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 0;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_CANT_BE_PC_SAME
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // LOAD16 immediate
         case 0b0000000011:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 1;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = BIT16_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // LOAD16 register address
         case 0b0000000100:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 0;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_CANT_BE_PC_SAME
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // LOAD32 register address
         case 0b0000000101:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 0;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
 
         // STORE8
         case 0b0000000110:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 1;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            unsigned char invld_b_val[] = {ZERO};
+            unsigned char invld_c_val[] = {PC};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                1,
+                &invld_b_val,
+                1,
+                &invld_c_val,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // STORE16
         case 0b0000000111:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 1;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            unsigned char invld_b_val[] = {ZERO};
+            unsigned char invld_c_val[] = {PC};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                1,
+                &invld_b_val,
+                1,
+                &invld_c_val,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         // STORE32
         case 0b0000001000:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 1;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            unsigned char invld_b_val[] = {ZERO};
+            unsigned char invld_c_val[] = {PC};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                1,
+                &invld_b_val,
+                1,
+                &invld_c_val,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
 
 
         // MOVE
         case 0b0000001001:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 0;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
 
 
-        // ADD
+        // ADD immediate
         case 0b1000000000:
-            self->reg_A = (source_code & (0b111111 << 16)) >> 16;
-            self->bit16_imme = 0;
-            self->mid_p = (source_code & (0b1111 << 12)) >> 12;
-            self->reg_B = (source_code & (0b111111 << 6)) >> 6;
-            self->C_is_imme = 0;
-            self->reg_C = (source_code & 0b111111);
+            self->imme_type = BIT10_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                1,
+                0,
+                NULL,
+                0,
+                NULL,
+                B_ISNT_REG
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
+            break;
+        // ADD register
+        case 0b1000000001:
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
+            break;
+        // SUB immediate (imme - reg)
+        case 0b1000000010:
+            self->imme_type = BIT10_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                1,
+                0,
+                NULL,
+                0,
+                NULL,
+                B_ISNT_REG
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
+            break;
+        // SUB immediate (reg - imme)
+        case 0b1000000011:
+            self->imme_type = BIT10_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                1,
+                0,
+                NULL,
+                0,
+                NULL,
+                B_ISNT_REG
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
+            break;
+        // SUB register
+        case 0b1000000100:
+            self->imme_type = NONE_IMME;
+
+            unsigned char invld_a_val[] = {PC, ZERO};
+            struct Checker checker = {
+                2,
+                &invld_a_val,
+                0,
+                0,
+                NULL,
+                0,
+                NULL,
+                BC_NO_CONST
+            };
+
+            struct CheckResult check_result = self->check(self, checker);
+            if (!check_result.check_pass) {
+                return check_result;
+            }
             break;
         default:
             break;
+        }
+    }
+
+    int meth_check_impl(struct Decoder *self, struct Checker checker) {
+        for (int i_a_c = 0; i_a_c < checker.a_invld_num; i_a_c++) {
+            if (self->reg_A == checker.a_invld_val[i_a_c]) {
+            }
         }
     }
 #endif
--- src/Decoder/decoder.h
+++ src/Decoder/decoder.h
@@ -1,17 +1,90 @@
 #ifdef __32bit__
+    #define PC      0b101001
+    #define ZERO    0b000000
+
+    #define A0  1
+    #define A1  2
+    #define A2  3
+    #define A3  4
+    #define AR0 5
+    #define AR1 6
+    #define AR2 7
+    #define ASS 8
+    #define ASP 9
+    #define ADS 10
+
+    #define B0  11
+    #define B1  12
+    #define B2  13
+    #define B3  14
+    #define BR0 15
+    #define BR1 16
+    #define BR2 17
+    #define BSS 18
+    #define BSP 19
+    #define BDS 20
+
+    #define C0  21
+    #define C1  22
+    #define C2  23
+    #define C3  24
+    #define CR0 25
+    #define CR1 26
+    #define CR2 27
+    #define CSS 28
+    #define CSP 29
+    #define CDS 30
+
+    #define D0  31
+    #define D1  32
+    #define D2  33
+    #define D3  34
+    #define DR0 35
+    #define DR1 36
+    #define DR2 37
+    #define DSS 38
+    #define DSP 39
+    #define DDS 40
+
+    #define BC_NO_CONST         0
+    #define BC_CANT_BE_PC_SAME  1
+    #define B_ISNT_REG          2
+
+    struct Checker {
+        int a_invld_num;
+        unsigned char *a_invld_val;
+        int p_vld;
+        int b_invld_num;
+        unsigned char *b_invld_val;
+        int c_invld_num;
+        unsigned char *c_invld_val;
+
+        unsigned char bc_const;
+    }
+
+    struct CheckResult {
+        int check_pass;
+        char *error_msg;
+    }
+
+    #define NONE_IMME   0
+    #define BIT16_IMME  2
+    #define BIT10_IMME  3
+
     struct Decoder {
         unsigned char reg_A;
 
-        unsigned char bit16_imme;
+        unsigned char imme_type;
         unsigned char mid_p;
         unsigned char reg_B;
-        unsigned char C_is_imme;
         unsigned char reg_C;
 
-        int (*decode)(struct Decoder *self,int source_code);
+        struct CheckResult (*decode)(struct Decoder *self, int source_code);
+        struct CheckResult (*check)(struct Decoder *self, struct Checker checker);
     }
 
     struct Decoder new_decoder();
 
-    int meth_decode_impl(struct Decoder *self, int source_code);
+    struct CheckResult meth_decode_impl(struct Decoder *self, int source_code);
+    struct CheckResult meth_check_impl(struct Decoder *self, struct Checker checker);
 #endif
